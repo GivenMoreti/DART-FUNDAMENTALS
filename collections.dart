d6b3c403591c3ce4c61320f61d0d4031e72ff9b7
Given <moretigiven@gmail.com>
2024-02-01 19:54:20 +0200

    constant sets

--- collections.dart
+++ collections.dart
@@ -5,16 +5,36 @@ void main() {
   int index = myFunc(nums);
   print(index.toString() + " got it");
 
-
   //To create a list that’s a compile-time constant, add const before the list literal:
 
   var constantList = const [1, 2, 3];
-  
+
+  //a Set in Dart is an unordered collection of unique items.
+
+  var names = {"jane", "willie", "jonathan", "Kane"};
+  names.add("jim"); //adds at the end of list
+  print(names.elementAt(0)); //gets the zeroeth element e.g jane.
+  // print(names);
+  print("names in a $names set");
+  for (int i = 0; i < names.length; i++) {
+    print(names.elementAt(i));
+  }
+
+  //empty sets
+  var emptySet = <String>{};
+  emptySet.add("Mike");
+  print(emptySet);
+
+  //create a constant set
+  //a set that doesnt allow add or remove
+  final myConstantSet = const {"jean", "bottle", "pens"};
+  // myConstantSet.add("speaker");  error
+  // myConstantSet.remove("bottle"); error
 }
 
 // create function
 int myFunc(var nums) {
-  for (int i = 0; i < nums.length; i++) { 
+  for (int i = 0; i < nums.length; i++) {
     if (nums[i] == 6) {
       return i;
     }
