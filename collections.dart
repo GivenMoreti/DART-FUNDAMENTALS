7c84b971f921ac8b0e9ea7ffe9c2c60ad342108b
Given <moretigiven@gmail.com>
2024-02-01 19:42:22 +0200

    custom functions

--- collections.dart
+++ collections.dart
@@ -2,7 +2,22 @@ void main() {
   //lists are similar to arrays
   var nums = [1, 4, 2];
 // printing individual list items
-  for (int i = 0; i < nums.length; i++) {
-    print(nums[i]);
+  int index = myFunc(nums);
+  print(index.toString() + " got it");
+
+
+  //To create a list that’s a compile-time constant, add const before the list literal:
+
+  var constantList = const [1, 2, 3];
+  
+}
+
+// create function
+int myFunc(var nums) {
+  for (int i = 0; i < nums.length; i++) { 
+    if (nums[i] == 6) {
+      return i;
+    }
   }
+  return -1;
 }
