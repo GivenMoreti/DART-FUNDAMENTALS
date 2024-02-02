c1c0b895799378361834b39077aa1dc7839a7918
Given <moretigiven@gmail.com>
2024-02-02 10:06:59 +0200

    exception handling stack trace

--- exceptionHandling.dart
+++ exceptionHandling.dart
@@ -5,5 +5,18 @@
  * 3. integerdivisionbyzero
  * 4.IOException-> BASE CALL INPUT OUTPUT
  * 5.IsolateSpawn -> an isolate cannot be created.
- * 6.Timeout ->
+ * 6.Timeout -> scheduled timeout while waiting for an async results.
  */
+void main() {
+  try {
+    int result = 12 ~/ 0;
+    print(result);
+  } on IntegerDivisionByZeroException {
+    //IntegerDivisionByZeroException
+    print("you cant divide by zero");
+  } catch (e, s) {  //s is for stack trace
+    //catch all cases
+    print(e);
+    print(s);
+  }
+}
